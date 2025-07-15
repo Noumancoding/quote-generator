39df080e50eb53f011ed7ce04a07c7f3b655d950
Nouman Coding <noumanali.coding@gmail.com>
2025-07-15 11:16:02 +0500

    add: select DOM elements for quote, author, and button

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // Quote logic will be implemented here
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
